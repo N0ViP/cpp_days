13da3d01a3dc6c6ca2fc5a9e1abab5d21b262f36
N0ViP <yjaafar.dev@gmail.com>
2026-07-02 16:56:57 +0100

    feat(C06): add ex01 and ex02 exercises
    - Added the initial setup for `ex01`, including its `Makefile` to manage the build process for the serialization exercise.
    - Implemented `ex02` to explore C++ polymorphism and runtime type identification using `dynamic_cast`.
        - Defined a `Base` class hierarchy with derived classes `A`, `B`, and `C` in `C06/ex02/Base.hpp`.
        - Provided a `generate()` function in `C06/ex02/Base.cpp` that randomly instantiates objects of types `A`, `B`, or `C`.
        - Implemented `identify()` functions (overloaded for pointers and references) in `C06/ex02/Base.cpp` to demonstrate type identification at runtime.
        - Included `C06/ex02/main.cpp` to showcase the functionality of `generate()` and `identify()` with example usage.
        - Added `C06/ex02/Makefile` to compile the `ex02` project.

---
+++ C06/ex02/main.cpp
@@ -0,0 +1,21 @@
+#include "Base.hpp"
+
+int main()
+{
+
+    std::srand(std::time(0));
+
+    Base *b1 = generate();
+    std::cout << "identify b1 type by pointer   : ";
+    identify(b1);
+    std::cout << "identify b1 type by reference : ";
+    identify(*b1);
+
+    std::cout << "========================================" << std::endl;
+
+    Base *b2 = generate();
+    std::cout << "identify b2 type by pointer   : ";
+    identify(b2);
+    std::cout << "identify b2 type by reference : ";
+    identify(*b2);
+}
